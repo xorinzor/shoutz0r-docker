c41e8f8bcf3e094ce8fae1aaeec48fc437955af8
xorinzor <4212335+xorinzor@users.noreply.github.com>
2020-06-01 10:35:09 +0200

    Modified docker-compose.yml

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#TODO make sure git and docker are installed
+
+#Download web files
+git clone https://github.com/xorinzor/shoutz0r-webui ./www
+
+#Download player files
+git clone https://github.com/xorinzor/shoutz0r-player ./player
+
+#TODO deploy docker-compose
